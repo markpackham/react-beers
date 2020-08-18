0e42c7d5991d8b72cb6983812221baed493b447b
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-08-18 18:09:49 +0100

    Initial commit@

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+function RandomBeer() {
+  const [name, setName] = useState();
+  const [volumeValue, setVolumeValue] = useState();
+  const [volumeUnit, setVolumeUnit] = useState();
+
+  useEffect(() => {
+    fetch("https://api.punkapi.com/v2/beers/random")
+      .then((results) => results.json())
+      .then((data) => {
+        setName(data[0].name);
+        const { volume } = data[0];
+        setVolumeValue(volume.value);
+        setVolumeUnit(volume.unit);
+      });
+    // the [] makes sure the data only gets fetched once
+  }, []);
+
+  return (
+    <div>
+      {name}: {volumeValue} {volumeUnit}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        React Beer Api learned from https://www.youtube.com/watch?v=Edw1Bq59VwU
+      </h1>
+      <RandomBeer />
     </div>
   );
 }
